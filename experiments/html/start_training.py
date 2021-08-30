d0ca7dca738ef50790bf8f596950c48636f64180
SaulLu <lucilesaul.com@gmail.com>
2021-08-30 09:40:13 +0000

    add strat_training_example_script

--- experiments/html/start_training.py
+++ experiments/html/start_training.py
@@ -1,18 +1,21 @@
+import sys
 from functools import partial
-from bsmetadata.experiments.with_metadata import get_dataloaders
-from bsmetadata.input_pipeline import DataConfig
-from bsmetadata.metadata_utils import (
-    add_metadata_and_chunk_examples,
-    create_global_metadata_prefix,
-    add_local_metadata_to_text,
-    chunks,
-)
-from bsmetadata.metadata_processors import PROCESSORS
-from transformers import AutoTokenizer
 
 from datasets import load_dataset
-
 from html_processor import HtmlProcessor, TagToRemove
+from transformers import AutoTokenizer
+
+from bsmetadata.experiments.with_metadata import get_dataloaders
+from bsmetadata.input_pipeline import DataConfig
+from bsmetadata.metadata_processors import PROCESSORS
+from bsmetadata.metadata_utils import (
+    add_local_metadata_to_text,
+    add_metadata_and_chunk_examples,
+    chunks,
+    create_global_metadata_prefix,
+)
+from bsmetadata.train import main, show_help
+
 
 tags_to_remove_alone = [
     TagToRemove("body"),
@@ -37,34 +40,41 @@ PROCESSORS["html"] = partial(
     ],
 )
 
-args = DataConfig(
-    train_file="/home/lucile/mini-html-parser/data/v1.0/pre-process-body-v3/nq-train-00.jsonl.gz",
-    extension="json",
-    metadata_list=["html"],
-    preprocessing_num_workers=8
-)
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
+# args = DataConfig(
+#     train_file="/home/lucile/mini-html-parser/data/v1.0/pre-process-body-v3/nq-train-00.jsonl.gz",
+#     extension="json",
+#     metadata_list=["html"],
+#     preprocessing_num_workers=8
+# )
+# tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+# # dataloaders = get_dataloaders(tokenizer, args)
+
+# # dataloaders
+
+# # train_dataloader = dataloaders[0]
+
+# # sample = next(iter(train_dataloader))
+# # print(tokenizer.convert_ids_to_tokens(sample["input_ids"][0]))
+# # dataset = load_dataset(args.extension, data_files=[args.train_file])
+
+# # # dataset["train"][0]
+
+# # examples = dataset["train"][:2]
+
+# # output = add_metadata_and_chunk_examples(examples=examples, tokenizer=tokenizer, cfg=args)
+
+
+# # print("******")
+# # print(tokenizer.decode(output["input_ids"][0]))
+
 
 # dataloaders = get_dataloaders(tokenizer, args)
+# print(list(next(iter(dataloaders[0])).keys()))
+# print(tokenizer.decode(next(iter(dataloaders[0]))["input_ids"][0]))
 
-# dataloaders
-
-# train_dataloader = dataloaders[0]
-
-# sample = next(iter(train_dataloader))
-# print(tokenizer.convert_ids_to_tokens(sample["input_ids"][0]))
-# dataset = load_dataset(args.extension, data_files=[args.train_file])
-
-# # dataset["train"][0]
-
-# examples = dataset["train"][:2]
-
-# output = add_metadata_and_chunk_examples(examples=examples, tokenizer=tokenizer, cfg=args)
-
-
-# print("******")
-# print(tokenizer.decode(output["input_ids"][0]))
-
-
-dataloaders = get_dataloaders(tokenizer, args)
-print(dataloaders)
+if __name__ == "__main__":
+    if "--help" in sys.argv or "-h" in sys.argv:
+        show_help()
+        sys.exit()
+    main()

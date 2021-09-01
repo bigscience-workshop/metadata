07b44d208cd1401369236a4b693aa290e3f267f5
SaulLu <lucilesaul.com@gmail.com>
2021-09-01 09:44:18 +0200

    replace squad by crime_and_punish

---
+++ experiments/html/SLURM/crime_and_punish_test/load_dataset.py
@@ -0,0 +1,88 @@
+import logging
+import sys
+from datasets import config
+
+import hydra
+from datasets import load_dataset
+from hydra.core.config_store import ConfigStore
+
+from bsmetadata.input_pipeline import DataConfig
+from bsmetadata.train import show_help
+
+logger = logging.getLogger(__name__)
+
+cs = ConfigStore.instance()
+cs.store(name="data_config", node=DataConfig)
+
+
+@hydra.main(config_name="data_config")
+def main(args: DataConfig) -> None:
+    data_files = {}
+    if args.train_file is not None:
+        data_files["train"] = args.train_file
+    if args.validation_file is not None:
+        data_files["validation"] = args.validation_file
+
+    if not data_files:
+        data_files = None
+
+    logger.info(config.HF_DATASETS_CACHE)
+    if args.dataset_name is not None:
+        logger.info(
+            "Downloading and loading a dataset from the hub"
+            f"{args.dataset_name}, {args.dataset_config_name}, data_files={data_files}, cache_dir={args.cache_dir},"
+        )
+        # Downloading and loading a dataset from the hub.
+        raw_datasets = load_dataset(
+            args.dataset_name,
+            args.dataset_config_name,
+            data_files=data_files,
+            cache_dir=args.cache_dir,
+            keep_in_memory=False,
+        )
+
+        if "validation" not in raw_datasets.keys():
+            logger.info("validation not in raw_datasets.keys()")
+            raw_datasets["validation"] = load_dataset(
+                args.dataset_name,
+                args.dataset_config_name,
+                split=f"train[:{args.validation_split_percentage}%]",
+                cache_dir=args.cache_dir,
+            )
+            raw_datasets["train"] = load_dataset(
+                args.dataset_name,
+                args.dataset_config_name,
+                split=f"train[{args.validation_split_percentage}%:]",
+                cache_dir=args.cache_dir,
+            )
+    else:
+        extension = args.train_file.split(".")[-1] if not args.extension else args.extension
+        if extension == "txt":
+            raise ValueError(
+                "You have entered a text file for the train data, but this type of file cannot contain metadata "
+                "columns. Wouldn't you rather have a file in json/jsonl or pandas format?"
+            )
+        if extension == "jsonl":
+            extension = "json"
+        raw_datasets = load_dataset(extension, data_files=data_files, cache_dir=args.cache_dir)
+
+        if "validation" not in raw_datasets.keys():
+            raw_datasets["validation"] = load_dataset(
+                extension,
+                data_files=data_files,
+                split=f"train[:{args.validation_split_percentage}%]",
+                cache_dir=args.cache_dir,
+            )
+            raw_datasets["train"] = load_dataset(
+                extension,
+                data_files=data_files,
+                split=f"train[{args.validation_split_percentage}%:]",
+                cache_dir=args.cache_dir,
+            )
+
+
+if __name__ == "__main__":
+    if "--help" in sys.argv or "-h" in sys.argv:
+        show_help()
+        sys.exit()
+    main()

5a94022f20b2a17158b550202de7dfa92345260f
SaulLu <lucilesaul.com@gmail.com>
2021-09-01 15:42:16 +0200

    change multi_batch

--- experiments/html/SLURM/experiment_1/multi_steps.bash
+++ experiments/html/SLURM/experiment_1/multi_steps.bash
@@ -1,3 +1,2 @@
-JID_JOB1=`sbatch  load_dataset.slurm | cut -d " " -f 4`
-JID_JOB2=`sbatch  --dependency=afterok:$JID_JOB1 create_dataset.slurm | cut -d " " -f 4`
-sbatch  --dependency=afterok:$JID_JOB2 do_training.slurm 
+JID_JOB1=`sbatch  create_dataset.slurm | cut -d " " -f 4`
+sbatch  --dependency=afterok:$JID_JOB1 do_training.slurm 

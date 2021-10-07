2941492766aab97592170b4c39d426ed643c281b
Timo Schick <schickt93@gmail.com>
2021-10-07 16:54:27 +0200

    Add preprocessing utils interface.

---
+++ bsmetadata/preprocessing_utils.py
@@ -0,0 +1,54 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This script provides functions for adding different kinds of metadata to a pretraining corpus.
+"""
+from abc import ABC, abstractmethod
+from typing import Dict, List, Optional
+
+
+class MetadataPreprocessor(ABC):
+    """A metadata processor can be used for preprocessing text and adding or extracting metadata information."""
+
+    @abstractmethod
+    def preprocess(self, examples: Dict[str, List]) -> Dict[str, List]:
+        """Process a batch of examples and add or extract corresponding metadata."""
+        pass
+
+
+class TimestampPreprocessor(MetadataPreprocessor):
+    """An exemplary metadata preprocessor for adding timestamp information based on URLs."""
+
+    def preprocess(self, examples: Dict[str, List]) -> Dict[str, List]:
+
+        example_metadata_list = examples["metadata"]
+
+        # Iterate through the metadata associated with all examples in this batch.
+        for example_metadata in example_metadata_list:
+            # Get the URL associated with this example.
+            example_urls = [md['value'] for md in example_metadata if md['key'] == 'url']
+
+            if not example_urls:
+                continue
+
+            # Try to extract a timestamp from the given URL and add it to the metadata.
+            example_timestamp = self._extract_timestamp_from_url(example_urls[0])
+
+            if example_timestamp:
+                example_metadata.append({'key': 'timestamp', 'type': 'global', 'value': example_timestamp})
+
+        return examples
+
+    def _extract_timestamp_from_url(self, url: str) -> Optional:
+        # This would have to be implemented.
+        return None

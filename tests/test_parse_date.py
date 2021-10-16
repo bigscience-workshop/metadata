c95b4042661ea75ba22d5dedc41657533aefd2ad
Jonathan Chang <ttjtftx@gmail.com>
2021-10-16 15:19:35 +0800

    Update test

--- tests/test_parse_date.py
+++ tests/test_parse_date.py
@@ -1,10 +1,10 @@
 import unittest
 from datetime import datetime as DateTime
 
-from bsmetadata.preprocessing_utils import get_path_from_url, parse_date
+from bsmetadata.preprocessing_utils import TimestampPreprocessor, parse_date
 
 
-class TestDateutl(unittest.TestCase):
+class TestDateutil(unittest.TestCase):
     def test_parse_works(self):
         date = parse_date("2021/01/23/random text and num 123 asdf")
         self.assertTrue(date == DateTime(2021, 1, 23))
@@ -25,6 +25,17 @@ class TestDateutl(unittest.TestCase):
         date = parse_date("2021/01")
         self.assertTrue(date is None)
         date = parse_date("2021 jan")
+        self.assertTrue(date is None)
+
+
+class TestTimestampPreprocessor(unittest.TestCase):
+    def test_extract_timestamp_from_url(self):
+        processor = TimestampPreprocessor()
+        url = "https://www.nytimes.com/1998/03/08/sports/on-pro-basketball-one-last-hurrah-for-the-bulls-reinsdorf-isn-t-quite-saying.html"
+        date = processor._extract_timestamp_from_url(url)
+        self.assertEqual(date, "1998-03-08 00:00:00")
+        url = "https://www.nytimes.com/sports/on-pro-basketball-one-last-hurrah-for-the-bulls-reinsdorf-isn-t-quite-saying.html"
+        date = processor._extract_timestamp_from_url(url)
         self.assertTrue(date is None)
 
 

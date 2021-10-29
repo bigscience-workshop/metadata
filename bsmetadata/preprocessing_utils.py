a92d5373b3bea4192c431fe0be06907c59603740
SaulLu <lucilesaul.com@gmail.com>
2021-10-29 16:38:28 +0200

    create HtmlPrepreprocessor

--- bsmetadata/preprocessing_utils.py
+++ bsmetadata/preprocessing_utils.py
@@ -15,7 +15,7 @@ This script provides functions for adding different kinds of metadata to a pretr
 """
 from abc import ABC, abstractmethod
 from typing import Dict, List, Optional
-
+from bsmetadata.preprocessing_tools import html_parser
 
 class MetadataPreprocessor(ABC):
     """A metadata processor can be used for preprocessing text and adding or extracting metadata information."""
@@ -52,3 +52,34 @@ class TimestampPreprocessor(MetadataPreprocessor):
     def _extract_timestamp_from_url(self, url: str) -> Optional:
         # This would have to be implemented.
         return None
+
+
+class HtmlPreprocessor(MetadataPreprocessor):
+    """todo"""
+
+    def preprocess(self, examples: Dict[str, List]) -> Dict[str, List]:
+        tags_to_remove_with_content = [
+            html_parser.objects.TagToRemoveWithContent(tag="script"),
+            html_parser.objects.TagToRemoveWithContent(tag="style"),
+            html_parser.objects.TagToRemoveWithContent(tag="header"),
+            html_parser.objects.TagToRemoveWithContent(tag="iframe"),
+            html_parser.objects.TagToRemoveWithContent(tag="footer"),  # copyright in footer
+            html_parser.objects.TagToRemoveWithContent(tag="form"),
+        ]
+        
+        examples["texts"] = [] # check key value
+        for example_doc_html, example_metadata in zip(examples["doc_html"], examples["metadata"]): # check key value
+
+            plain_text, metadata = html_parser.get_clean_text_and_metadata(
+                example_doc_html,
+                tags_to_remove_with_content=tags_to_remove_with_content,
+                consecutive_tags_to_fold=["div"],
+                convert_br_tag_to_breaking_line=True,
+            )
+            examples["texts"].append(plain_text)
+            example_metadata.extend([
+            html_parser.objects.convert_html_metadata_dataclass_to_dict(node) for node in metadata
+        ])
+
+        return examples
+        

0299c8886277cd0f2633ff8018270471a35935c3
Shanya Sharma - s0s0cr3 <Shanya.Sharma@walmartlabs.com>
2021-11-02 00:07:55 +0530

    add test

---
+++ tests/mocks/dump_db.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class DumpDB:
+    def __init__(self, path_to_dump_db="") -> None:
+        return
+
+    def redirects(self) -> List[tuple]:
+        return [{"a": "A"}, {"test.com", "Test"}]
+
+    def get_paragraphs(self, title: str):
+        return [{"text": "Paragraph 1"}, {"text": "Paragraph 2"}]
---
+++ tests/test_preprocessing_utils.py
@@ -0,0 +1,20 @@
+import unittest
+from unittest import mock
+
+from mocks.dump_db import DumpDB
+
+from bsmetadata.preprocessing_utils import WebsiteDescPreprocessor
+
+
+class WebsiteDescPreprocessorTester(unittest.TestCase):
+    def setUp(self) -> None:
+        self.html_processor = WebsiteDescPreprocessor()
+
+    @mock.patch("bsmetadata.preprocessing_utils.DumpDB")
+    def test_website_preprocessing(self, mock_db):
+        mock_db.return_value = DumpDB
+        print(mock_db)
+
+
+if __name__ == "__main__":
+    unittest.main()

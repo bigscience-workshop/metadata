fe9a22825a8c0dbba4be87569fe96d79da1f1870
Shanya Sharma - s0s0cr3 <Shanya.Sharma@walmartlabs.com>
2021-11-02 12:47:35 +0530

    Update download_wiki_dump.sh

--- bsmetadata/preprocessing_scripts/download_wiki_dump.sh
+++ bsmetadata/preprocessing_scripts/download_wiki_dump.sh
@@ -1,11 +1,11 @@
 
 
-${1:-../preprocessing_data} # default director: preprocessing_data
+out_dir=${1:-bsmetadata/preprocessing_data} # default director: preprocessing_data
 
 ## Clone the huggingface dataset repo containing wiki dump
-mkdir $1
+mkdir -p "$out_dir"
 HUB_REPO_NAME=bs-modeling-metadata/wiki_dump
-git clone https://huggingface.co/datasets/${HUB_REPO_NAME} $1
+git clone https://huggingface.co/datasets/${HUB_REPO_NAME} $out_dir/wiki_dump
 
 
 ## Downloading nltk punkt to be used in sentence tokenizer

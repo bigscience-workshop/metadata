640d1ec7e6e04523e986537e1d00ae0235e703a7
SaulLu <lucilesaul.com@gmail.com>
2021-11-04 18:36:58 +0100

    add local test to create dataset

---
+++ experiments/jz/html/exp_1/test_local/create_dataset.sh
@@ -0,0 +1,3 @@
+# change proba
+python experiments/jz/html/exp_1/start_training.py \
+    --config-dir="/mnt/storage/Documents/hugging_face/bigscience/jz/jz-code/sync/metadata/experiments/jz/html/exp_1/test_local/"

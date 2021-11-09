823b5e41c5d860ead17e905f8e0ea406b06d4435
SaulLu <lucilesaul.com@gmail.com>
2021-11-09 16:41:32 +0100

    change test local exp

--- experiments/jz/html/exp_1/test_local/create_dataset.sh
+++ experiments/jz/html/exp_1/test_local/create_dataset.sh
@@ -6,6 +6,8 @@ echo "$BASEDIR"
 python experiments/jz/html/exp_1/start_training.py \
     --config-dir=$BASEDIR \
     data_config.train_file="/mnt/storage/Documents/hugging_face/bigscience/jz/jz-code/sync/metadata/experiments/jz/html/exp_1/test_local/inputs/sample.json" \
+    data_config.validation_file=null \
     out_dir="/mnt/storage/Documents/hugging_face/bigscience/jz/jz-code/sync/metadata/experiments/jz/html/exp_1/test_local/outputs" \
+    data_config.overwrite_cache=true \
     do_train=false \
     do_eval=false 

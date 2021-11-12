86b4a7be57365e235f2ee7de55f3eed0d17224fd
chkla <chkla@users.noreply.github.com>
2021-11-12 16:10:43 +0100

    tests for the DatasourcePreprocessor

---
+++ tests/test_datasource_preprocessor.py
@@ -0,0 +1,42 @@
+import unittest
+from unittest import mock
+from datasets import Dataset
+
+from bsmetadata.preprocessing_utils import DatasourcePreprocessor
+
+class TestDatasourcePreprocessor(unittest.TestCase):
+    def test_extract_datasource(self):
+        my_dict = {
+            "id": [0, 1, 2],
+            "url": [
+                "https://seattlejob.us/finance-internships-miami.html",
+                "https://hawkeyesports.com/news/2005/03/22/on-time-and-on-budget/",
+                "http://www.plumberscrib.com/steam-generators.html?cat=142&manufacturer=390&p=2",
+                ],
+            "metadata": [[], [], []],
+        }
+     
+        target_id = [0, 1, 2]
+        target_url = [
+            "https://seattlejob.us/finance-internships-miami.html",
+            "https://hawkeyesports.com/news/2005/03/22/on-time-and-on-budget/",
+            "http://www.plumberscrib.com/steam-generators.html?cat=142&manufacturer=390&p=2",
+        ]
+        
+        target_metadata = [
+            [{"key": "length", "type": "global", "value": "seattlejob.us > finance internships miami html"}],
+            [{"key": "length", "type": "global", "value": "hawkeyesports.com > news > on time and on budget"}],
+            [{"key": "length", "type": "global", "value": "www.plumberscrib.com > steam generators html"}],
+        ]
+
+        processor = DatasourcePreprocessor()
+        
+        ds = Dataset.from_dict(my_dict)
+        ds = ds.map(lambda ex: processor.preprocess(ex), batched=True, batch_size=3)
+        
+        self.assertEqual(ds[:]["id"], target_id)
+        self.assertEqual(ds[:]["url"], target_text)
+        self.assertEqual(ds[:]["metadata"], target_metadata)
+
+if __name__ == "__main__":
+    unittest.main()

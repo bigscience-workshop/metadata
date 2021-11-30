cc8a3c4d301ca3950372ab61ac8430a9f863c3eb
SaulLu <lucilesaul.com@gmail.com>
2021-11-30 20:05:07 +0100

    create 1000 folder

---
+++ experiments/jz/dataset/c4/toy-dataset-1000/clean_directories.sh
@@ -0,0 +1,13 @@
+PROCESS_DIR=$SCRATCH/new_dataset/c4-en-reduced-in-progess-1000
+OUT_DIR_1=$PROCESS_DIR/c4-en-reduced-with-metadata-timestamp
+OUT_DIR_2=$PROCESS_DIR/c4-en-reduced-with-metadata-timestamp-website_description
+OUT_DIR_3=$PROCESS_DIR/c4-en-reduced-with-metadata-timestamp-website_description-entity
+
+cd OUT_DIR_1
+rm *
+
+cd OUT_DIR_2
+rm *
+
+cd OUT_DIR_3
+rm *
---
+++ experiments/jz/dataset/c4/toy-dataset-1000/multistep_dataset_creation.sh
@@ -0,0 +1,64 @@
+DATASET_FILES_DIR_INIT=$SCRATCH/new_dataset/c4-en-reduced-1000
+
+PROCESS_DIR=$SCRATCH/new_dataset/c4-en-reduced-in-progess-1000
+OUT_DIR_1=$PROCESS_DIR/c4-en-reduced-with-metadata-timestamp
+OUT_DIR_2=$PROCESS_DIR/c4-en-reduced-with-metadata-timestamp-website_description
+OUT_DIR_3=$PROCESS_DIR/c4-en-reduced-with-metadata-timestamp-website_description-entity
+
+mkdir $PROCESS_DIR
+mkdir $OUT_DIR_1
+mkdir $OUT_DIR_2
+mkdir $OUT_DIR_3
+
+PATH_OR_URL_FLAIR_NER_MODEL=$SCRATCH/cache_dir/flair/ner-fast/en-ner-fast-conll03-v0.4.pt
+
+for filename in $DATASET_FILES_DIR_INIT/*; do
+    echo "filename=$filename"
+    FILENAME="${filename##*/}"
+    FILENAME_END="${FILENAME: -4}" # test if this is a lock file
+    NEW_FILENAME="${FILENAME::-3}l" #   the last 3 characters correspond to the compression algorithm format .gz
+
+    if [ $FILENAME_END != "lock" ]; then    
+        # Create job to add timestamp metadata
+        DATASET_FILES_DIR=$DATASET_FILES_DIR_INIT
+        METADATA_TO_INCLUDE="['timestamp']"
+        OUT_DIR=$OUT_DIR_1
+
+        ID_JOB1=$(sbatch --job-name=modelling-metadata-c4-dataset-toy-add-metadata-timestamp \
+        --export=ALL,FILENAME=$FILENAME,NEW_FILENAME=$NEW_FILENAME,OUT_DIR=$OUT_DIR,METADATA_TO_INCLUDE=$METADATA_TO_INCLUDE,DATASET_FILES_DIR=$DATASET_FILES_DIR,PATH_OR_URL_FLAIR_NER_MODEL=$PATH_OR_URL_FLAIR_NER_MODEL \
+        02_add_metadata_to_toy_c4_dataset.slurm | cut -d " " -f 4)
+
+        echo "Launch jobid $ID_JOB1 to extract timestamp from $DATASET_FILES_DIR/$FILENAME and save to $OUT_DIR/$NEW_FILENAME"
+
+        # Create job to add website_description metadata
+        METADATA_TO_INCLUDE="['website_description']"
+        DATASET_FILES_DIR=$OUT_DIR
+        OUT_DIR=$OUT_DIR_2
+
+        NEW_FILENAME="$NEW_FILENAME.gz"
+
+        ID_JOB2=$(sbatch --dependency=afterok:$ID_JOB1 --job-name=modelling-metadata-c4-dataset-toy-add-metadata-website_description \
+        --export=ALL,FILENAME=$NEW_FILENAME,NEW_FILENAME=$NEW_FILENAME,OUT_DIR=$OUT_DIR,METADATA_TO_INCLUDE=$METADATA_TO_INCLUDE,DATASET_FILES_DIR=$DATASET_FILES_DIR,PATH_OR_URL_FLAIR_NER_MODEL=$PATH_OR_URL_FLAIR_NER_MODEL \
+        02_add_metadata_to_toy_c4_dataset.slurm | cut -d " " -f 4)
+
+        echo "Launch jobid $ID_JOB2 to extract website_description from $DATASET_FILES_DIR/$FILENAME and save to $OUT_DIR/$NEW_FILENAME"
+
+        # Create job to add entity metadata
+        METADATA_TO_INCLUDE="['entity']"
+        DATASET_FILES_DIR=$OUT_DIR
+        OUT_DIR=$OUT_DIR_3
+
+        ID_JOB3=$(sbatch --dependency=afterok:$ID_JOB2 --job-name=modelling-metadata-c4-dataset-toy-add-metadata-entity \
+        --qos=qos_gpu-t3 \
+        --account=six@gpu \
+        --gres=gpu:1 \
+        --constraint=v100-16g \
+        --partition=gpu_p13 \
+        --export=ALL,FILENAME=$NEW_FILENAME,NEW_FILENAME=$NEW_FILENAME,OUT_DIR=$OUT_DIR,METADATA_TO_INCLUDE=$METADATA_TO_INCLUDE,DATASET_FILES_DIR=$DATASET_FILES_DIR,PATH_OR_URL_FLAIR_NER_MODEL=$PATH_OR_URL_FLAIR_NER_MODEL \
+        02_add_metadata_to_toy_c4_dataset.slurm | cut -d " " -f 4)
+
+        echo "Launch jobid $ID_JOB3 to extract entity from $DATASET_FILES_DIR/$FILENAME and save to $OUT_DIR/$NEW_FILENAME"
+    fi
+done
+
+

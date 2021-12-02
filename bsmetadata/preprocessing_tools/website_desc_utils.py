5fc4e20d7c557e46b3c5ced839985c758e9ae0bd
SaulLu <lucilesaul.com@gmail.com>
2021-12-02 11:00:34 +0100

    reduce try/except scope into `fetch_wikipedia_description_for_title`

--- bsmetadata/preprocessing_tools/website_desc_utils.py
+++ bsmetadata/preprocessing_tools/website_desc_utils.py
@@ -23,10 +23,12 @@ class WebsiteDescUtils:
     def fetch_wikipedia_description_for_title(self, title: str) -> Optional:
         try:
             text = self.wiki_dump_db.get_paragraphs(title)[0].text
-            text = re.sub(r"\((?:[^)(]|\([^)(]*\))*\)", "", text)
-            text = nltk.sent_tokenize(text)[0]  # Picking the first sentence
-        except Exception:
+        except KeyError:
+            # In case the title has not corresponding paragraph
             return None
+
+        text = re.sub(r"\((?:[^)(]|\([^)(]*\))*\)", "", text)
+        text = nltk.sent_tokenize(text)[0]  # Picking the first sentence
         return text
 
     def extract_wiki_desc(self, keyword: str) -> Optional:

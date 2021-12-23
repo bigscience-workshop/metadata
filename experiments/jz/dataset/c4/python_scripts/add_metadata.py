661af7560b89ca0e83d6978810ab06b08600fa55
SaulLu <lucilesaul.com@gmail.com>
2021-12-23 16:49:48 +0100

    add_metadata script

---
+++ experiments/jz/dataset/c4/python_scripts/add_metadata.py
@@ -0,0 +1,230 @@
+import logging
+import os
+import sys
+from dataclasses import dataclass, field
+from functools import partial
+from typing import Dict, List, Optional, Tuple
+
+from datasets import Dataset, Features, Value
+import wandb
+import hydra
+from datasets import config, load_dataset
+from hydra.core.config_store import ConfigStore
+from omegaconf import OmegaConf
+
+from bsmetadata.preprocessing_utils import (
+    EntityPreprocessor,
+    MetadataPreprocessor,
+    TimestampPreprocessor,
+    WebsiteDescPreprocessor,
+)
+from bsmetadata.train import show_help
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class PreprocessingConfig:
+    file_name: str = field(metadata={"help": "The input file name(a jsonl file, eventually compressed)."})
+    out_file_name: str = field(metadata={"help": "The output file name(a jsonl file)."})
+    out_dir: str = field(metadata={"help": "where to save the resulting dataset."})
+    path_wiki_db: str = field(
+        metadata={"help": "The path to the wikipedia database file necessary for the website descriptions"}
+    )
+    entity_path_data_dir: str = field(
+        metadata={
+            "help": "The path to the directory containing the directories `ed-wiki-2019`, `generic` and `wiki_2019` "
+        }
+    )
+    path_or_url_flair_ner_model: Optional[str] = field(
+        default=None, metadata={"help": "TThe path or name of the flair ner model to use to preprocess entities"}
+    )
+    metadata_to_include: Optional[list] = field(
+        default_factory=lambda: ["website_description", "entity", "timestamp"],
+        metadata={"help": "The list of metadata to extract"},
+    )
+    dataset_name: Optional[str] = field(
+        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)"}
+    )
+    dataset_config_name: Optional[str] = field(
+        default=None, metadata={"help": "The configuration name of the dataset to use (via the datasets library)"}
+    )
+    overwrite_cache: Optional[bool] = field(
+        default=False, metadata={"help": "Whether the local cache containing datasets should be overwritten."}
+    )
+    cache_dir: Optional[str] = field(
+        default=None, metadata={"help": "Where do you want to store the pretrained models downloaded from s3?"}
+    )
+    preprocessing_num_workers: Optional[int] = field(
+        default=None, metadata={"help": "The number of processes to use for the preprocessing."}
+    )
+    map_batch_size: Optional[int] = field(
+        default=1,
+        metadata={
+            "help": "This is the size of the batch size that will be used for the mapping operation when generating"
+            " the dataset. If you are using `with_metadata` the recommended batch size is 1.."
+        },
+    )
+    project_name: str = field(default="metadata_lm_exploration", metadata={"help": "The project name."})
+
+
+class Logger:
+    def __init__(self, *args, **kwargs):
+        self.run = wandb.init(*args, **kwargs)
+
+    def log(self, dic):
+        wandb.log(dic)
+
+    def close(self):
+        wandb.finish()
+
+
+cs = ConfigStore.instance()
+cs.store(name="preprocessing_config", node=PreprocessingConfig)
+
+
+def add_url_as_metadata(examples: Dict[str, List], column_name_url: str = "url") -> Dict[str, List]:
+
+    example_url_list = examples[column_name_url]
+    example_metadata = []
+    example_urls = []
+
+    for example_url in example_url_list:
+        example_metadata.append([{"key": "url", "type": "global", "value": example_url}])
+        example_urls.append(example_url)
+
+    examples["metadata"] = example_metadata
+    examples["id"] = example_urls  # to change
+    return examples
+
+
+col_url = "url"
+col_to_store_text = "text"
+col_to_store_metadata_html = "metadata_html"
+col_to_store_metadata_url = "metadata_url"
+col_to_store_metadata_timestamp = "metadata_timestamp"
+col_to_store_metadata_website_desc = "metadata_website_desc"
+col_to_store_metadata_entities = "metadata_entity"
+col_to_store_metadata_generation_length_text = "metadata_generation_length_text"
+col_to_store_metadata_generation_length_sentence = "metadata_generation_length_sentence"
+col_to_store_metadata_datasource = "metadata_generation_datasource"
+
+
+@hydra.main(config_name="preprocessing_config")
+def main(args: PreprocessingConfig) -> None:
+    data_files = {"file": args.file_name}
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+
+    config_dict = OmegaConf.to_container(args)
+    metrics_logger = Logger(project=args.project_name, config=config_dict)
+
+    logger.info(config.HF_DATASETS_CACHE)
+    logger.info(
+        "Downloading and loading a dataset from the hub"
+        f"{args.dataset_name}, {args.dataset_config_name}, data_files={data_files}, cache_dir={args.cache_dir},"
+    )
+    # Downloading and loading a dataset from the hub.
+
+    metrics_logger.log({"load_dataset": 0})
+    ds = load_dataset(
+        args.dataset_name,
+        args.dataset_config_name,
+        data_files=data_files,
+        cache_dir=args.cache_dir,
+        keep_in_memory=False,
+        download_mode="force_redownload",
+    )["file"]
+
+    metrics_logger.log({"load_dataset": 1})
+
+    features_dict = dict(ds.features)
+
+    def apply_processor(ds: Dataset, processor: MetadataPreprocessor) -> Dataset:
+        for col_name, feature_type in processor.new_columns_minimal_features.items():
+            assert col_name not in features_dict
+            features_dict[col_name] = feature_type
+        extraction_name = processor.__class__.__name__
+
+        logger.info(f"Start {extraction_name}")
+        metrics_logger.log({extraction_name: 0})
+        ds.map(
+            processor.preprocess,
+            batched=True,
+            batch_size=args.map_batch_size,
+            num_proc=args.preprocessing_num_workers,
+            load_from_cache_file=not args.overwrite_cache,
+            desc=f"Running {extraction_name} on dataset",
+            features=Features(features_dict),
+        )
+        metrics_logger.log({extraction_name: 1})
+        logger.info(f"End {extraction_name}")
+        return ds
+
+    if "html" in args.metadata_to_include:
+        html_processor = HtmlPreprocessor(
+            col_to_store_metadata=col_to_store_metadata_html, col_to_store_text=col_to_store_text
+        )
+        ds = apply_processor(ds=ds, processor=html_processor)
+
+    if "url" in args.metadata_to_include:
+        url_processor = UrlPreprocessor(col_to_store_metadata=col_to_store_metadata_url, col_url=col_url)
+        ds = apply_processor(ds=ds, processor=url_processor)
+
+    if "timestamp" in args.metadata_to_include:
+        timestamp_processor = TimestampPreprocessor(
+            col_to_store_metadata=col_to_store_metadata_timestamp, col_metadata_url=col_to_store_metadata_url
+        )
+        ds = apply_processor(ds=ds, processor=timestamp_processor)
+
+    if "website_description" in args.metadata_to_include:
+        website_processor = WebsiteDescPreprocessor(
+            col_to_store_metadata=col_to_store_metadata_website_desc, col_metadata_url=col_to_store_metadata_url
+        )
+        ds = apply_processor(ds=ds, processor=website_processor)
+
+    if "entity" in args.metadata_to_include:
+        entity_processor = EntityPreprocessor(
+            base_url=args.entity_path_data_dir,
+            path_wiki_db=args.path_wiki_db,
+            path_or_url_flair_ner_model=args.path_or_url_flair_ner_model,
+            col_to_store_metadata=col_to_store_metadata_entities,
+            col_text=col_to_store_text,
+        )
+        ds = apply_processor(ds=ds, processor=entity_processor)
+
+    if "generation_length_text" in args.metadata_to_include:
+        generation_length_preprocessor_text = GenerationLengthPreprocessor(
+            mode="text", col_to_store_metadata=col_to_store_metadata_generation_length_text
+        )
+        ds = apply_processor(ds=ds, processor=generation_length_preprocessor_text)
+    
+    if "generation_length_sentence" in args.metadata_to_include:
+        generation_length_preprocessor_sentence = GenerationLengthPreprocessor(
+            mode="sentence", col_to_store_metadata=col_to_store_metadata_generation_length_sentence
+        )
+        ds = apply_processor(ds=ds, processor=generation_length_preprocessor_sentence)
+
+    if "datasource" in args.metadata_to_include:
+        datasource_preprocessor = DatasourcePreprocessor(
+            col_to_store_metadata=col_to_store_metadata_datasource, col_url="url"
+        )
+        ds = apply_processor(ds=ds, processor=datasource_preprocessor)
+
+    saving_path = os.path.join(args.out_dir, args.out_file_name)
+    logger.info(f"Save resulting dataset at {saving_path}")
+    ds.to_json(saving_path)
+    metrics_logger.close()
+
+
+if __name__ == "__main__":
+    if "--help" in sys.argv or "-h" in sys.argv:
+        show_help()
+        sys.exit()
+    main()

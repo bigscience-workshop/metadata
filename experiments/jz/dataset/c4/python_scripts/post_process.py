918b17ce763d96b47eb5be2c78251b8ccd0cca49
Jonathan Chang <ttjtftx@gmail.com>
2022-06-06 19:53:05 +0800

    Make style

--- experiments/jz/dataset/c4/python_scripts/post_process.py
+++ experiments/jz/dataset/c4/python_scripts/post_process.py
@@ -7,11 +7,11 @@ from typing import Optional
 
 import datasets
 import hydra
-import wandb
 from datasets import Dataset, Features, config, load_dataset, load_from_disk
 from hydra.core.config_store import ConfigStore
 from omegaconf import OmegaConf
 
+import wandb
 from bsmetadata.post_processing_utils import MetadataPostProcessor, WebsiteDescPostProcessor
 from bsmetadata.train import show_help
 

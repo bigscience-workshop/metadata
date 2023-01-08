7f668efa83c97db86e2cdcd71e9e962f85a6b6aa
Jonathan Chang <ttjtftx@gmail.com>
2023-01-08 21:02:44 +0800

    Fix mask bug

--- bsmetadata/train.py
+++ bsmetadata/train.py
@@ -136,7 +136,7 @@ def loss_fn(batch, outputs, metadata_mask=None):
     shift_logits = lm_logits[..., :-1, :].contiguous()
     shift_labels = labels[..., 1:].contiguous()
     if metadata_mask is not None:
-        loss_mask = torch.logical_and(attention_mask, ~metadata_mask)
+        loss_mask = torch.logical_and(attention_mask, ~(metadata_mask.bool()))
     else:
         loss_mask = attention_mask
     shift_mask = loss_mask[..., 1:].contiguous()

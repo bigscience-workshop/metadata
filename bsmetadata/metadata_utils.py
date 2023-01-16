1b8c6879d7e1d0e439ff8434276db5a96800f15a
Jonathan Chang <ttjtftx@gmail.com>
2023-01-16 23:24:04 +0800

    WIP

--- bsmetadata/metadata_utils.py
+++ bsmetadata/metadata_utils.py
@@ -208,7 +208,7 @@ def random_sample_metadata_v2(
     Returns:
         A new collection of examples, with some metadata dropped.
     """
-    only_metadata_types = list(metadata_type_sample_weights.keys())
+    only_metadata_types = [key for key in metadata_type_sample_weights.keys() if f"metadata_{key}" in examples]
     for i in range(len(examples["text"])):
         example = {k: v[i] for k, v in examples.items()}
         metadata_types = [key for key in only_metadata_types if example[f"metadata_{key}"]]
